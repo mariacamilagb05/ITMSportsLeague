f7aa0c60c9abab025e5e70588e27f323bf05113a
mariacamilagb05 <mariacamilagb05@gmail.com>
2026-04-04 12:35:36 -0500

    Add TournamentSponsorRepository implementation

---
+++ SportsLeague.DataAccess/Repositories/TournamentSponsorRepository.cs
@@ -0,0 +1,29 @@
+﻿using SportsLeague.DataAccess.Context;
+using SportsLeague.Domain.Entities;
+using SportsLeague.Domain.Interfaces.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace SportsLeague.DataAccess.Repositories
+{
+    public class TournamentSponsorRepository : GenericRepository<TournamentSponsor>, ITournamentSponsorRepository
+    {
+        public TournamentSponsorRepository(LeagueDbContext context) : base(context)
+        {
+        }
+
+        public async Task<TournamentSponsor?> GetByTournamentAndSponsorAsync(int tournamentId, int sponsorId)
+        {
+            return await _dbSet
+                .Where(ts => ts.TournamentId == tournamentId && ts.SponsorId == sponsorId)
+                .FirstOrDefaultAsync();
+        }
+
+        public async Task<IEnumerable<TournamentSponsor>> GetByTournamentIdAsync(int tournamentId)
+        {
+            return await _dbSet
+                .Where(ts => ts.TournamentId == tournamentId)
+                .Include(ts => ts.Sponsor)
+                .ToListAsync();
+        }
+    }
+}
