2d17b4d0b2f9f557a1456fe8e7b2c818b41d79c0
mariacamilagb05 <mariacamilagb05@gmail.com>
2026-04-04 11:33:07 -0500

    Add ISponsorRepository and ISponsorService interfaces

---
+++ SportsLeague.Domain/Interfaces/Repositories/ISponsorRepository.cs
@@ -0,0 +1,13 @@
+﻿using SportsLeague.Domain.Entities;
+using SportsLeague.Domain.Enums;
+
+namespace SportsLeague.Domain.Interfaces.Repositories
+{
+    public interface ISponsorRepository : IGenericRepository<Sponsor>
+    {
+        Task<IEnumerable<Sponsor>> GetByCategoryAsync(SponsorCategory category);
+        Task<Sponsor?> GetByIdWithTournamentAsync(int id);
+        Task<bool> ExistsByNameAsync(string name);
+        Task<Sponsor?> GetByNameAsync(string name);
+    }
+}
---
+++ SportsLeague.Domain/Interfaces/Services/ISponsorService.cs
@@ -0,0 +1,17 @@
+﻿using SportsLeague.Domain.Entities;
+
+namespace SportsLeague.Domain.Interfaces.Services
+{
+    public interface ISponsorService
+    {
+        Task<IEnumerable<Sponsor>> GetAllAsync();
+        Task<Sponsor?> GetByIdAsync(int id);
+        Task<Sponsor> CreateAsync(Sponsor sponsor);
+        Task UpdateAsync(int id, Sponsor sponsor);
+        Task DeleteAsync(int id);
+        Task AddSponsorToTournamentAsync(int tournamentId, int sponsorId);
+        Task RemoveSponsorFromTournamentAsync(int tournamentId, int sponsorId);
+        Task<IEnumerable<Tournament>> GetTournamentsBySponsorAsync(int sponsorId);
+
+    }
+}
