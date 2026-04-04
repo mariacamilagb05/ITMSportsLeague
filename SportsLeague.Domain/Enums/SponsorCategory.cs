434b80775cb67fd04c907514e8dcdd68a65b7a3c
mariacamilagb05 <mariacamilagb05@gmail.com>
2026-04-04 09:20:47 -0500

    Add Sponsor entity

--- SportsLeague.Domain/Enums/SponsorCategory.cs
+++ SportsLeague.Domain/Enums/SponsorCategory.cs
@@ -2,6 +2,7 @@
 {
     public enum SponsorCategory
     {
+        Unknown = -1,  // Desconocido o no especificado
         Main = 0,      // Patrocinador principal
         Gold = 1,      // Oro
         Silver = 2,    // Plata
