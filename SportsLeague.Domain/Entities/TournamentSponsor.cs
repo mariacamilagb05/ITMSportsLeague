3e53eeb215a369d1896459aadc4e748aa7f93806
mariacamilagb05 <mariacamilagb05@gmail.com>
2026-04-04 09:21:32 -0500

    Add TournamentSponsor join entity

---
+++ SportsLeague.Domain/Entities/TournamentSponsor.cs
@@ -0,0 +1,14 @@
+﻿namespace SportsLeague.Domain.Entities
+{
+    internal class TournamentSponsor : AuditBase
+    {
+        public int TournamentId { get; set; }
+        public int SponsorId { get; set; }
+        public decimal ContractAmount { get; set; }
+        public DateTime JoinedAt { get; set; } = DateTime.UtcNow;
+
+        //Navigation properties
+        public Tournament Tournament { get; set; } = null!;
+        public Sponsor Sponsor { get; set; } = null!;
+    }
+}
