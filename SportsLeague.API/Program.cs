cffaaa55b29b7c34e729c2e0d6492816f10676e9
mariacamilagb05 <mariacamilagb05@gmail.com>
2026-05-19 12:10:29 -0500

    Register StandingsService in DI container

--- SportsLeague.API/Program.cs
+++ SportsLeague.API/Program.cs
@@ -38,6 +38,8 @@ builder.Services.AddScoped<ISponsorService, SponsorService>();
 builder.Services.AddScoped<IMatchService, MatchService>();
 builder.Services.AddScoped<IMatchEventService, MatchEventService>();
 builder.Services.AddScoped<MatchValidationHelper>();
+builder.Services.AddScoped<IStandingsService, StandingsService>();
+
 
 
 // ── AutoMapper ──
